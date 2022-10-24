c6a379f2c0352ed1da2aea813c59c618426cbcee
김지수 <wltn2858@swu.ac.kr>
2022-10-24 14:54:58 +0900

    add backend directory

---
+++ backend/algorithm-site/src/main/java/com/soori/algorithmsite/AlgorithmSiteApplication.java
@@ -0,0 +1,13 @@
+package com.soori.algorithmsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmSiteApplication.class, args);
+	}
+
+}
