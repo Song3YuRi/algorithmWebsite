87c2e7055c6ff1df5e594a4901646e5b8d8608c1
김지수 <wltn2858@swu.ac.kr>
2022-10-24 14:54:58 +0900

    add backend directory

---
+++ backend/algorithm-site/src/test/java/com/soori/algorithmsite/AlgorithmSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soori.algorithmsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
