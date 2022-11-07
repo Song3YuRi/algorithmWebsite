17cb2047ffb6810535a925df157bf9fc5e755b8b
김지수 <wltn2858@swu.ac.kr>
2022-11-07 21:10:15 +0900

    feat: Modify signUp method in controller

--- backend/algorithm-site/src/main/java/com/soori/algorithmsite/user/controller/UserController.java
+++ backend/algorithm-site/src/main/java/com/soori/algorithmsite/user/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.soori.algorithmsite.user.controller;
 
+import com.soori.algorithmsite.user.DTO.UserDeleteDTO;
+import com.soori.algorithmsite.user.DTO.UserSignUpDTO;
 import com.soori.algorithmsite.user.User;
 import com.soori.algorithmsite.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,8 +20,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signUp")
-    User signUp(User user) {
-        return userService.signUp(user);
+    User signUp(UserSignUpDTO userSignUpDTO) {
+        return userService.signUp(userSignUpDTO);
     }
 
 }
